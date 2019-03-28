060f1b6eacd365518847f7ce93803c366355d18f
Crystalline Emerald <crystalline.emerald@gmail.com>
2019-03-28 19:23:55 +0300

    sync works

--- options.js
+++ options.js
@@ -1,9 +1,25 @@
-console.log('tabhelper init!')
+console.log('tabhelper init at', new Date())
 
 const pr = console.log;
 
 const settings = {
   version: '0.1.0'  
+}
+
+function formatDate(d) {
+  if (typeof d === 'number') {
+    d = new Date(d);
+  }
+  const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+  return `${d.getDay()} ${months[d.getMonth()]} ${d.getFullYear()}, ${d.getHours().toString().padStart(2)}:${d.getMinutes().toString().padStart(2)}:${d.getSeconds().toString().padStart(2)}`;
+}
+
+function pick(obj, keys) {
+  var ret = {};
+  for (var i=0; i<keys.length; i++) {
+    ret[keys[i]] = obj[keys[i]];
+  }
+  return ret;
 }
 
 class App {
@@ -12,6 +28,8 @@ class App {
     this.searchQuery = false;
     this.options = {};
     this.renderer = () => {};
+    this.synced = false;
+    this.displayPopup = false;
   }
   
   connectRenderer(renderer, mountNode) {
@@ -23,16 +41,55 @@ class App {
     this.renderer(this, this.mountNode);
   };
   
+  // sync state to local storage, and to remote server, if enabled
+  sync(doneCb) {
+    persistToKey('tab-helper-data', pick(this, ['tabs', 'options']), (err) => {
+      if (!err) {
+        this.synced = true;
+      }
+      doneCb && doneCb(err);
+    });
+  }
+  
+  restore(doneCb) {
+    restoreFromKey('tab-helper-data', (val, err) => {
+      pr(val,err);
+      if (!err) {
+        Object.assign(this, val);
+        this.synced = true;
+      }
+      doneCb && doneCb(err); 
+    });
+  };
+  
   saveTabs(doneCb) {
     getTabs((newTabs => {
       Object.keys(newTabs).forEach(windowId => {
         const tabGroup = {windowId: windowId, date: Date.now(), tabs: newTabs[windowId]};
         this.tabs.push(tabGroup);
       });
+      this.synced = false;
       this.render();
+      this.sync(() => {
+        this.render();
+      });
       doneCb && doneCb(this);
     }));
   };
+}
+
+function persistToKey(key, val, doneCb) {
+  chrome.storage.local.set({
+    [key]: val
+  }, (err) => {
+    doneCb && doneCb(err);
+  }); 
+}
+
+function restoreFromKey(key, doneCb) {
+  chrome.storage.local.get([key], (val, err) => {
+    doneCb && doneCb(val && val[key], err);
+  }); 
 }
 
 const model = new App();
@@ -49,13 +106,13 @@ function copyToClipboard(text) {
 };
 
 function getTabs(callback) {
-  var queryInfo = {
+  const queryInfo = {
   };
 
   chrome.tabs.query(queryInfo, function(tabs) {
-    var _windows = {};
+    const _windows = {};
     tabs.forEach(t => {
-      var id = t.windowId;
+      const id = t.windowId;
       pr(id);
       if (!_windows[id]) _windows[id] = [];
       _windows[id].push({
@@ -95,13 +152,38 @@ function render(model, mountNode) {
   let tabListView = '';
   
   model.tabs.forEach(tabGroup => {
-    tabListView += '<div class="tab-gr">';
-    tabListView += '<div class="tab-gr-header"></div>';
-    tabListView += '<div class="tab-gr-list">';
+    let popup = '';
+    
+    if (this.displayPopup) {
+      const actions = this.displayPopup.actions || ['ok', 'cancel']
+      popup = `
+        <div class='popup-fs'>
+          <div class='popup-header'></div>
+          <div class='popup-text'></div>
+          <div class='popup-actions'>
+            ${actions.map(a => `<div data-action="${a}"class='action'>${a}</div>`).join(' ')}
+          </div>
+        </div>
+      `
+    }
+    
+    tabListView += `
+      ${popup}
+      <div class="tab-gr-c">
+      <div class="tab-gr group">
+        <div class="tab-gr-header">
+          <div class="small-label">${formatDate(tabGroup.date)}</div>
+          <div class="small-button">Restore</div>
+          <div class="small-button">Export</div>
+          <div class="small-button">Delete</div>
+        </div>
+        <div class="tab-gr-list">`;
+    
     tabGroup.tabs.forEach(tab => {
-      tabListView += `<div class="tab-item" href="${tab.url}">${tab.title}</div>`;
+      tabListView += `<div class="tab-item">&#8226;&nbsp;<a class="tab-link" href="${tab.url}">${tab.title}</a></div>`;
     });
-    tabListView += '</div></div>';
+    
+    tabListView += `</div></div></div>`;
   });
   
   mountNode.innerHTML = `
@@ -115,8 +197,8 @@ function render(model, mountNode) {
       <div class='button' id='options'>Options</div>
     </div>
     ${model.synced ?
-      "<div class='indicator ind-green'>Synced</div>" :
-      "<div class='indicator ind-red'>Not synced</div>"
+      "<div style='width: 90px; display: flex; justify-content: center;' class='label green'>Synced</div>" :
+      "<div style='width: 90px; display: flex; justify-content: center;' class='label red'>Not synced</div>"
     }
   </div>
   <div class='tabs'>${tabListView}</div>
@@ -127,25 +209,9 @@ function render(model, mountNode) {
   });
 }
 
-function saveData(model, doneCb) {
-  chrome.storage.local.set({
-      tabs: model.tabs
-  }, (err) => {
-    doneCb && doneCb(err, model);
-  });
-}
-
-function getData(model, doneCb) {
-  chrome.storage.local.get('tabs', (tabs, err) => {
-    if (!err) {
-      model.tabs = tabs;  
-    }
-    doneCb && doneCb(err, model);
-  });
-}
-
 function startApp() {
   model.tabs = [
+  /*
     {
      date: Date.now(),
      tabs: [
@@ -153,11 +219,16 @@ function startApp() {
       {url: 'https://wikipedia.com', title: 'wikipedia'},
      ]
     }
+  */
   ];
   
   model.connectRenderer(render, $('#root-node')[0]);
   
   model.render();
+  
+  model.restore((err) => {
+    model.render();
+  });
 }
 
 document.addEventListener('DOMContentLoaded', startApp);
